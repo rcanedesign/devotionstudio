ec6baa5c0fd3a5e78cc24db6ac8efeb11b1cc1b2
kaiakirsten <kaiakirsten@yahoo.com>
2014-08-24 23:14:05 -0700

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,3 +1,11 @@
 				<div id="content">
-					<h2>Coming as soon as Kirsten's Husband helps her with it ;)</h2>
+					<a href="https://www.facebook.com/pages/Devotion-Studio/180995271945875"><h2>Check out our Facebook page and photos</h2></a>
+					
+					
+					<br>
+					
+					<a href="http://www.snapfish.com/snapfish/shareereg/p=7179131400036947713/l=6333125027/g=221413027/cobrandOid=1000/otsc=SHR/otsi=SALBFB/pns/snapfish/share/p=7179131400036947713/l=6333125027/g=221413027/cobrandOid=1000/otsc=SHR/otsi=SALBFB">
+					<h2>Check out our Snapfish photos</h2>
+					<br>
+					<img src="https://www.facebook.com/180995271945875/photos/pb.180995271945875.-2207520000.1408947038./794246253954104/?type=3&theater"></a>
 					</div>
