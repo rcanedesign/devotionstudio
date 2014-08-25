df092ba89fe20cda389edbce71ecbf8985b55240
kaiakirsten <kaiakirsten@yahoo.com>
2014-08-24 22:40:30 -0700

    Update home.php

--- home.php
+++ home.php
@@ -42,25 +42,9 @@ Check us out at YELP!<br>
 							<div class="section">
 								<h2>Welcome to Devotion Studio</h2>
 								<p>
-								Devotion Studio is an owner operated, appointment only, intimate salon. Owners Kelli and Kirsten are Sebastopol raised, determined young women. Devotion Studio was opened with much thought and determination to make a clean and comfortable environment for our guests. We do all of our services with our guests happiness as our top priority. No matter what it is you need, you will feel comfortable in knowing that you are in our hands. We specialize in wedding hair and makeup artistry. Beautifying is a gift that the both of us were blessed with. Try us out, you will not be disappointed!
+								Devotion Studio is an owner operated, appointment only, salon. Owner, Kirsten, is a Sebastopol raised, determined young woman. Devotion Studio was opened with much thought and determination to make a clean and comfortable environment for our guests. Having my own studio for 3 years I decided to focus solely on weddings and getting rid of a physical location. I now travel to you! I have a few other talented stylists and makeup artists I work with. We do all of our services with our guests happiness as our top priority. No matter what it is you need, you will feel comfortable in knowing that you are in our hands. We specialize in wedding hair and makeup artistry. Beautifying is a gift that we were blessed with. Try us out, you will not be disappointed!
 </p>
-<p>
-Hair color and texture services are done with Pravana products. Pravana Naturceuticals ushers in an entirely new era in hair care.Their highly sophisticated formulas utilize both the power of nature and the strength of technology to provide a new hybrid of superior hair care products which are in effect, super-natural.
-</p>
-<p>
-Pravanas unique brand infuses all shampoos and conditioners with Pravana's Proprietary Naturceutical Complex, comprised of nine time-proven Meso-American botanicals known for their therapeutic and healing properties teamed with three different hydrolyzed proteins to enhance hair's strength, elasticity, and shine from the inside out. Pravana's Shampoo are exclusively sulfate, sulfite, and salt-free. All Pravana hair care formulas are also free of cocamide DEA and MEA, and contain no phthalates or other hazardous ingredients. They employ no animal testing or animal ingredients in their products.
-</p>
-<p>
-Perfection Smooth Out<br>
-How does it work?<br>
-Perfection's proprietary Nano-Amino Complex miraculously suspends the internal textural memory of the hair to safely transform it to a new, soft and sensual look and feel.<br>
-How long does it last?<br>
-By following the Perfection SmoothOut service with regular use of the Perfection Smoothing sulfate and salt-free Shampoo and Conditioner, the hair will retain its new sleek and smooth memory state for 8-12 weeks. It will then gradually return to its pre-service state with no line of demarcation.<br>
-Who is the Perfection SmoothOut Service for?<br>
-The Perfection SmoothOut Solution has a low pH formula and contains no harsh chemicals, which make it safe to use on all clients. Unlike the trouble-filled Thio and Brazilian-type straighteners of recent past, Perfection's advanced formula poses no health risk and actually improves the integrity of the hair.<br>
-Can the Perfection SmoothOut service be applied over hair that is color-treated, highlighted and/or bleached?<br>
-The Perfection SmoothOut service can be successfully applied to color-treated, highlighted and/or bleached hair. In fact, Perfection is so gentle it allows for same-day color and shampoo.<br>
-</p>
+
 								
 							</div>
 							
