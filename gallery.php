3d9b5d1377f732b86452de099994d2c77176e9a9
kaiakirsten <kaiakirsten@yahoo.com>
2014-08-24 23:15:14 -0700

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -7,5 +7,5 @@
 					<a href="http://www.snapfish.com/snapfish/shareereg/p=7179131400036947713/l=6333125027/g=221413027/cobrandOid=1000/otsc=SHR/otsi=SALBFB/pns/snapfish/share/p=7179131400036947713/l=6333125027/g=221413027/cobrandOid=1000/otsc=SHR/otsi=SALBFB">
 					<h2>Check out our Snapfish photos</h2>
 					<br>
-					<img src="https://www.facebook.com/180995271945875/photos/pb.180995271945875.-2207520000.1408947038./794246253954104/?type=3&theater"></a>
+					<img src="https://scontent-a-sjc.xx.fbcdn.net/hphotos-xpa1/t1.0-9/10534597_794246253954104_4751098099597446708_n.jpg"></a>
 					</div>
